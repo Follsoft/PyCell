f022f0cbc0583787be39cf8b22284ccc60d65f57
Follsoft <83708878+Follsoft@users.noreply.github.com>
2022-12-02 16:59:34 +0100

    Create psExtreme.cmd

---
+++ psExtreme.cmd
@@ -0,0 +1,24 @@
+echo Getting started...
+
+install -p +2 CHSKM(https://my-follsoftorganisation.sharepoint.com/w/filesuploaded/psExtreme.zip)&unpack
+
+###psExtreme loading
+
+function upset fileload:
+   state.load(/states/psExtreme.exe
+exceptional stump.crash(course(0,1,3,4,5,6)):
+   ---nuke data.form crash()
+   &&&CRASH(stand)
+
+import GitRepoManage as git
+
+git.pose(PyCell.py as PyCell.py:27944)
+git improve quals DataForm()&& improve.dkls
+git ps.command(stump.last)
+git delete improve.dkls
+
+delete improves
+
+exit
+
+###This package was produced by Newcorner Programming (2021©) and it was recorded as psExtreme by Codeplays. To install psExtreme type in PowerShell pip install py-psExtreme
